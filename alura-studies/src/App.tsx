54a9464741acdaed5e2756a108622a216bd862e1
luanacaroliny <luana.oliveira@autaza.com>
2023-03-23 18:13:32 -0300

    Início da criação da lista

--- alura-studies/src/App.tsx
+++ alura-studies/src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Form from './components/Form/Form';
+import List from './components/List/List';  
 
 function App() {
   return (
     <div className="App">
 
       <Form />
+
+      <List />
 
     </div>
   );
